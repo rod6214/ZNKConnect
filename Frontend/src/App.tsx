b05a297a1a5dd23548099b6588b0953d0cca3b96
Nelson Amador <rod6214@gmail.com>
2019-09-28 10:13:15 -0300

    [Frontend] - Added react app

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+// class Example extends React.Component {
+//   public Render() {
+//     return (<div>cddcdcdccdcdc</div>);
+//   }
+// }
+
+const App: React.FC = () => {
+  return (<div>cdcdcdc</div>);
+}
+
+export default App;
---
+++ Frontend/src/Example.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export class Example extends React.Component {
+
+    public render() {
+        return (<div>clkddcdncdkc</div>);
+    }
+}
