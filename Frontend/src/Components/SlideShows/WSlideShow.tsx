91708fa0a6190856d886d6bd438f22c68371a728
Nelson Amador <rod6214@gmail.com>
2019-09-29 00:13:17 -0300

    [Frontend] - Finished slideshow

--- Frontend/src/Components/SlideShows/WSlideShow.tsx
+++ Frontend/src/Components/SlideShows/WSlideShow.tsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import { jsxAttribute } from '@babel/types';
+import './WSlideShow.css';
 
 export interface WSlideShowProperty {
     slideItems: ToadObject[];
     children?: React.ReactNode[] | React.ReactNode;
-}
-
-interface WSlideShowState {
-    imageId: number;
-}
-
-interface ToadProperty {
-    toad?: ToadObject;
 }
 
 export interface ToadObject {
@@ -26,20 +18,19 @@ class WSlideShow extends React.Component {
 
     private _slideAnimationHandle?: number;
     private _elements: React.ReactNode[] = [];
-    private _slideContent?: React.RefObject<HTMLDivElement>
+    private _slideContent?: React.RefObject<HTMLDivElement>;
+    private _counter: number;
 
     public constructor(public props: WSlideShowProperty) {
         super(props);
         this.state = {imageId: 0};
         this._slideContent = React.createRef();
+        this._counter = 0;
     }
-
-    // componentWillMount(): void {
-    //     // this._elements = this.props.slideItems.map(item => (<Toad toad = {item}/>));
-    // }
 
     componentDidMount(): void {
         if (!this._slideAnimationHandle) {
+            this._Changer();
             this._slideAnimationHandle = window.setInterval(() => this._Changer(), 4000);
         }
     }
@@ -55,14 +46,11 @@ class WSlideShow extends React.Component {
                 const element = children.item(i) as HTMLDivElement;
                 element.style.display = "none";
             }
+            if (this._counter > children.length - 1) {this._counter = 0}
+            const element = children.item(this._counter) as HTMLDivElement;
+            element.style.display = "block";
+            this._counter++;
         }
-        // const state = this.state as WSlideShowState;
-        // if (state.imageId > this.props.slideItems.length - 2) {
-        //     this.setState({imageId: 0});
-        // }
-        // else {
-        //     this.setState({imageId: state.imageId + 1});
-        // }
     }
     
     render(): React.ReactNode {
@@ -83,51 +71,7 @@ class WSlideShow extends React.Component {
                 {toadObject}
             </div>
         );
-        // return (
-        //     // <div className="ntc-slides" ref={this._slideContent}>
-        //     //     {/* <!-- Automatic Slideshow Images --> */}
-        //     //     <div className="mySlides w3-display-container w3-center">
-        //     //         <img src="https://www.w3schools.com/w3images/la.jpg" style={{width:'100%'}} />
-        //     //         <div className="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
-        //     //         <h3>Los Angeles</h3>
-        //     //         <p><b>We had the best time playing at Venice Beach!</b></p>   
-        //     //         </div>
-        //     //     </div>
-        //     //     <div className="mySlides w3-display-container w3-center">
-        //     //         <img src="https://www.w3schools.com/w3images/ny.jpg" style={{width:'100%'}} />
-        //     //         <div className="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
-        //     //         <h3>New York</h3>
-        //     //         <p><b>The atmosphere in New York is lorem ipsum.</b></p>    
-        //     //         </div>
-        //     //     </div>
-        //     //     <div className="mySlides w3-display-container w3-center">
-        //     //         <img src="https://www.w3schools.com/w3images/chicago.jpg" style={{width:'100%'}} />
-        //     //         <div className="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
-        //     //         <h3>Chicago</h3>
-        //     //         <p><b>Thank you, Chicago - A night we won't forget.</b></p>    
-        //     //         </div>
-        //     //     </div>
-        //     // </div>
-        // );
     }
 }
-
-// function Toad(props: ToadProperty) {
-//     const toad = props.toad;
-//     if (toad) {
-//         return (
-//             <div className="mySlides w3-display-container w3-center">
-//                 <img src={toad.imgSrc} style={{width:'100%'}}/>
-//                 <div className="w3-display-bottommiddle w3-container w3-text-white w3-padding-32 w3-hide-small">
-//                     <h3>{toad.title}</h3>
-//                     <p><b>{toad.description}</b></p>    
-//                 </div>
-//             </div>
-//         );
-//     }
-//     else {
-//         return (<div></div>);
-//     }
-// }
 
 export default WSlideShow;
